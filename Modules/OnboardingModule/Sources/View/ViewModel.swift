0b500eb48ccec5d516f23991b382561a250ce1a1
ahmad995alawneh <ahmad.cras95@gmail.com>
2025-02-19 17:44:29 +0300

    Updating files

---
+++ Modules/OnboardingModule/Sources/View/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Untitled.swift
+//  Pods
+//
+//  Created by Ahmad Alawneh on 19/02/2025.
+//
+
+import Foundation
+
+
+class OnboardingViewModel: ObservableObject {
+    @Published var data: Data?
+    @Published var error: String?
+
+}
